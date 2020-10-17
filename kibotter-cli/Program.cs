a565102392fbab65fe91664d822fd0c471f4940a
きーぼう <kibou811@gmail.com>
2020-10-17 13:43:55 +0900

    add project files

---
+++ kibotter-cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace kibotter_cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
